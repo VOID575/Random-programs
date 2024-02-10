c28c59a0ba336cd4228f304db07e7eb49e1703a9
VOID575 <155201815+VOID575@users.noreply.github.com>
2024-02-10 18:07:53 +0100

    Update csv_module.py

--- csv_module.py
+++ csv_module.py
@@ -6,26 +6,26 @@ fichier_csv = 'blackbox.csv'
 # Fonction pour ajouter une nouvelle valeur au fichier CSV
 def ajouter_valeur(valeur):
    
-    # Ouvrir le fichier CSV en mode ajout
+    # Ouvre le fichier CSV en mode ajout
     with open(fichier_csv, 'a') as fichier:
         
-        # Écrire la nouvelle valeur dans le fichier
+        # Écrit la nouvelle valeur dans le fichier
         fichier.write(str(valeur) + '\n')
 
-# Fonction pour maintenir un maximum de 30 valeurs dans le fichier CSV
+# Fonction pour maintenir un maximum de 85 valeurs dans le fichier CSV
 def gerer_limitation_valeurs():
-    # Lire toutes les valeurs du fichier CSV dans une liste
+    # Lit toutes les valeurs du fichier CSV dans une liste
     valeurs = []
     with open(fichier_csv, 'r') as fichier:
         for ligne in fichier:
             valeurs.append(ligne.strip())
             print(len(valeurs))
     
-    # Si le nombre de valeurs dépasse 85, supprimer les premières valeurs
+    # Si le nombre de valeurs dépasse 85, cela supprime les premières valeurs
     if len(valeurs) >= 80:
         valeurs = valeurs[-80:]
     
-    # Réécrire le fichier CSV avec les valeurs mises à jour
+    # Réécrit le fichier CSV avec les valeurs mises à jour
     with open(fichier_csv, 'w') as fichier:
         for valeur in valeurs:
             fichier.write(str(valeur) + '\n')
