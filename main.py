05fe507d639b3e53276884aa182a2103eb740c84
VOID575 <155201815+VOID575@users.noreply.github.com>
2024-02-10 18:00:14 +0100

    Update main.py
    commentaires

--- main.py
+++ main.py
@@ -5,34 +5,37 @@ from pimoroni_i2c import PimoroniI2C
 from csv_module import * 
 
 PINS_I2C_BME688 = {"sda": 4, "scl": 5}
-
 i2c = PimoroniI2C(**PINS_I2C_BME688)
+#J'associe les les pins de sorties du capteur BME688 à une variable pour pouvoir récupérer les données
 bme = BreakoutBME68X(i2c)
 
 while True:
     
-    #définitions des variable, elles renvoient toutes un int équivalent à leur unité de mesure
+    #Définitions des variable, elles renvoient toutes un int équivalent à leur unité de mesure
     temperature, pressure, humidity, gas, status, _, _,= bme.read()
-    heater = "Stable" if status & STATUS_HEATER_STABLE else "Unstable"
-    life_t = "good" if temperature > 50 and temperature < -50 else "critical"
-    #NB : Si la pression est trop basse alors il peut y avoir un risque de météo dangereuse
-    life_p = "good" if pressure > 92000 and pressure < 160000 else "critical" 
-    life_h = "good" if humidity > 40 and humidity < 70 else "critical"
-    life_g = "good" if gas < 100000 else "critical"
     
+    #définition du time stamp
     date = utime.localtime(utime.time())
  
-    #J'écris toute les mesures
+    #J'écris toutes les mesures séparément :
+    #D'abord j'écris le time stamp
     ajouter_valeur(f"{date[0]}/{date[1]}/{date[2]} à {date[3]}h{date[4]}min{date[5]}sec :")
+    #La température
     ajouter_valeur(f"{round(temperature,2)}c")
+    #la pression
     ajouter_valeur(f"{round(pressure,2)}Pa")
+    #L'humidité
     ajouter_valeur(f"{round(humidity,2)}%")
+    #La quantité de microparticule
     ajouter_valeur(f"{round(gas,2)}Ohms")
+    #Je vérifie si le programme ne fait pas plus de 85 lignes pour qu'il ne prenne pas trop de place
     gerer_limitation_valeurs()
-    
+
+    #j'affiche les données dans la console
     print(f"{date[0]}/{date[1]}/{date[2]} à {date[3]}h{date[4]}min{date[5]}sec : {round(temperature,2)}c, {round(pressure,2)}Pa, {round(humidity,2)}%, {round(gas,2)} Ohms\n")
-    
-    time.sleep(0.2)
+
+    #Je fais une pause de 1 seconde
+    time.sleep(1)
 
     
         
